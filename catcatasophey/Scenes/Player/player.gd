7ad4819615ca1710a392f5f4e75a936a2c461440
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-07-11 17:33:29 -0600

    [Major] Started component system/ State Machine
    + Moved files around
    + Started Componets
    + Started State Machines

---
+++ catcatasophey/Scenes/Player/player.gd
@@ -0,0 +1,33 @@
+class_name Player
+extends CharacterBody2D
+
+@onready var state_machine: StateMachine = $StateMachine
+
+#region Resources
+@export var stats: Resource
+#endregion
+
+#region Components
+@onready var input_component: InputComponent = %InputComponent
+@onready var move_component: MovementComponent = %MovementComponent
+@onready var animation_component: AnimationComponent = %AnimationComponent
+@onready var damage_component: DamageComponent = %DamageComponent
+@onready var hurtbox_component: HurtboxComponent = %HurtboxComponent
+#endregion
+
+func _ready() -> void:
+	state_machine.init(self)
+	damage_component.init_damage_component(self, hurtbox_component)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	state_machine.process_input(event)
+
+
+func _process(delta: float) -> void:
+	state_machine.process_frame(delta)
+
+
+func _physics_process(delta: float) -> void:
+	state_machine.process_physics(delta)
+	move_and_slide()
--- catcatasophey/Scripts/Componets/input_component.gd
+++ catcatasophey/Scripts/Componets/input_component.gd
@@ -1,8 +1,8 @@
 class_name InputComponent
 extends Node
 
-func get_direction_input() -> Vector2:
-	return Input.get_vector("move_left","move_right","move_up","move_down")
+func get_direction_input() -> float:
+	return Input.get_axis("move_left","move_right")
 
 func get_attack_input() -> bool:
 	return Input.is_action_just_pressed("attack")
