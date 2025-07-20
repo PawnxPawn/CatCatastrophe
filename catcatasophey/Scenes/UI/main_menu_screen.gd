9a5622328e97361ec424a6245bbbf5680d676bf3
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-07-20 00:19:31 -0600

    [Major]]MainMenu

--- catcatasophey/Scenes/UI/main_menu_screen.gd
+++ catcatasophey/Scenes/UI/main_menu_screen.gd
@@ -1,1 +1,20 @@
-extends Control
+extends CanvasLayer
+
+@onready var quit_button: TextureButton = $QuitButton
+@onready var credits_button: TextureButton = $CreditsButton
+@onready var options_button: TextureButton = $OptionsButton
+@onready var new_game_button: TextureButton = $NewGameButton
+
+func _ready() -> void:
+	GlobalAudio.set_track(&"mainmenu")
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/credits.tscn")
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Maps/level design.tscn")
--- catcatasophey/Scenes/UI/pause_screen.gd
+++ catcatasophey/Scenes/UI/pause_screen.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	GlobalAudio.set_track(&"credits")
+	if GlobalInfo.has_won == true:
+		GlobalAudio.set_track(&"credits")
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_anything_pressed():
 		get_tree().change_scene_to_file("res://Scenes/UI/main_menu_screen.tscn")
+		
