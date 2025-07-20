bdbcefc0ae69e4f1a95b56831986a7b5da257494
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-07-20 08:34:09 -0600

    [HotFix] Hitbox component timer

--- catcatasophey/Scripts/Componets/hitbox_component.gd
+++ catcatasophey/Scripts/Componets/hitbox_component.gd
@@ -4,23 +4,46 @@ extends Area2D
 signal hitbox_hit
 
 @export var damage: int = 1
-@export var damage_repeat_time: float = 1.0
+@export var damage_repeat_time: float = 5.0
 @export var is_enviorment_detectable = false
+@export var walkable_hazard = false
+
 
 @onready var timer: Timer = Timer.new()
 
+var current_hurtbox: HurtboxComponent = null
 
 
 func _ready():
 	area_entered.connect(_on_hurtbox_entered)
+	area_exited.connect(_on_hurtbox_exited)
 	body_entered.connect(_on_enviorment_hit)
+	timer.wait_time = damage_repeat_time
+	timer.timeout.connect(deal_damage)
+	timer.one_shot = false
+
+
+func deal_damage() -> void:
+	current_hurtbox.emit_hurt(self)
 
 
 func _on_enviorment_hit(_enviorment: Node2D) -> void:
 	if is_enviorment_detectable:
 		hitbox_hit.emit()
 
+
 func _on_hurtbox_entered(hurtbox: Node2D):
 	if not hurtbox is HurtboxComponent: return
 	hurtbox.emit_hurt(self)
 	hitbox_hit.emit()
+	if walkable_hazard == true:
+		current_hurtbox = hurtbox
+		add_child(timer)
+		timer.start()
+
+
+func _on_hurtbox_exited(area:Node2D) -> void:
+	current_hurtbox = null
+	if walkable_hazard == true:
+		timer.stop()
+		remove_child(timer)
